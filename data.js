b7c21b5bf44c1527612a75838082ef1d9bcd7e63
Julie Bissø <U2507981@rts-365.dk>
2025-02-04 09:13:40 +0100

    opg 1 gennemført

---
+++ data.js
@@ -0,0 +1,14 @@
+let cat ={
+    firstname: "fluffy",
+    nickname: "old lady",
+    age: 13,
+    hometown: "brønshøj"
+}
+
+ //-----opgave 1-----
+console.log(cat.firstname)
+console.log(cat.nickname)
+console.log(cat.age)
+console.log(cat.hometown)
+
+//-----opgave 2-----
