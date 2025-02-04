be826954e65703a7fcc997ab477911713df13c7c
Julie Bissø <U2507981@rts-365.dk>
2025-02-04 09:22:04 +0100

    opg 2 gennemført

--- data.js
+++ data.js
@@ -2,13 +2,28 @@ let cat ={
     firstname: "fluffy",
     nickname: "old lady",
     age: 13,
-    hometown: "brønshøj"
+    hobbies: ["sleeping", "yelling", "running", "looking at builders"],
+    hometown: "brønshøj",
+    family:{
+parent: "cat mom",
+enemy: "ib"
+    }
 }
 
  //-----opgave 1-----
-console.log(cat.firstname)
+/* console.log(cat.firstname)
 console.log(cat.nickname)
 console.log(cat.age)
 console.log(cat.hometown)
+ */
 
 //-----opgave 2-----
+
+cat.hobbies.forEach(function(hobby){
+    console.log(hobby);
+})
+
+console.log(cat.family.parent);
+console.log(cat.family.enemy);
+
+//-----opgave 3-----
